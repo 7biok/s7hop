c26bbd898a829ecc5f4e82c6e2ade186116baa39
7biok <52315005+7biok@users.noreply.github.com>
2020-04-01 23:01:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 admin_id = 644951528 #ChtaId от вашкго тг, получить у getmyid_bot
 
-token = '1124609538:AAGD-vUVjDs3MCTkqjbpex2uUrEiXCZtvy0' #Токен от tg бота
+token = '1070605225:AAE6-kN59ivDaRB2lEJRB2qKfgWklglqnhQ' #Токен от tg бота
